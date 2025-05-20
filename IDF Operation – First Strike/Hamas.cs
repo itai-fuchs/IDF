8a51f38bbb94ea0ff96616b159219e83bd46d6a6
davidkupferstin <k6780519@gmail.com>
2025-05-20 19:22:07 +0300

    crait class Hamas

---
+++ IDF Operation – First Strike/Hamas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation___First_Strike
+{
+    internal class Hamas
+    {
+        String Formation = "";
+        String Commander = "";
+        List<String> AffiliatedTerrorists = ["","",""];
+    }
+}
